1ca7d01ce4d537e0e066c31ca85c4d6aaa65d75b
Etienne Silvio <40568278+etiensilvio205@users.noreply.github.com>
2018-12-18 11:55:13 +0400

    Update checkuser.php

--- checkuser.php
+++ checkuser.php
@@ -10,7 +10,7 @@ $password="";
 $name="";
 
 
-$conn = new mysqli("localhost","root","","boutique");
+$conn = new mysqli("eu-cdbr-west-02.cleardb.net","b15e6359a294ee","604a07fd","heroku_f0451ca8c2ee3d5");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
